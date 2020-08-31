98f5170ffb8d2d9a5d308c38637d2a191ad44b11
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-08-31 16:39:55 -0600

    adding angular material, test with navbar

--- store/src/app/modules/core/core.module.ts
+++ store/src/app/modules/core/core.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AuthService } from './services/auth.service';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  imports: [BrowserModule, RouterModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    HttpClientModule,
+    MatToolbarModule,
+  ],
   providers: [AuthService],
   declarations: [NavbarComponent],
   exports: [NavbarComponent],
