5a22e4c29fc493f2984ecc3824f19be58f84a728
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-09-02 16:36:05 -0600

    fixing img scaling bug on home page

--- store/src/app/app.module.ts
+++ store/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './modules/core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, AppRoutingModule],
+  imports: [CoreModule, AppRoutingModule, StoreModule.forRoot(reducers, {
+      metaReducers
+    })],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- store/src/app/modules/core/core.module.ts
+++ store/src/app/modules/core/core.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
