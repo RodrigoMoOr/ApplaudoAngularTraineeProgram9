aca414f5fbc7659b3d8180b579a0ea731f340e8a
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-09-01 23:28:39 -0600

    product details page

--- store/src/app/modules/features/product/components/product-layout/product-layout.component.ts
+++ store/src/app/modules/features/product/components/product-layout/product-layout.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { IProduct } from './../../../../shared/interfaces/product-response.interface';
+import { ProductsService } from './../../../../shared/services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-layout',
   templateUrl: './product-layout.component.html',
-  styleUrls: ['./product-layout.component.scss']
+  styleUrls: ['./product-layout.component.scss'],
 })
 export class ProductLayoutComponent implements OnInit {
+  product: IProduct;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) {}
 
   ngOnInit(): void {
+    this.getRouteParams();
   }
 
+  getRouteParams(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.getProduct(params.get('name'));
+    });
+  }
+
+  getProduct(slug: string) {
+    this.productsService
+      .getBySlug(slug, 'image_attachment.blob,category,master')
+      .subscribe((response) => {
+        this.product = response.data;
+      });
+  }
 }
