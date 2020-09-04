adc351675bb5abfef540da3abc0c03c6d5b3b4e2
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-09-04 12:34:13 -0600

    core module completed

--- Challenge/store/src/app/app.module.ts
+++ Challenge/store/src/app/app.module.ts
@@ -1,25 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './modules/core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers
-    })
-  ],
+  declarations: [AppComponent],
+  imports: [CoreModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
