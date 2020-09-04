c5976be69e894e80c219ed6e8798ef9be7ece44f
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-09-04 13:02:23 -0600

    shared module completed

--- Challenge/store/src/app/modules/shared/shared.module.ts
+++ Challenge/store/src/app/modules/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
